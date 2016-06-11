bbcb394ab346d2e8f2f30a3e3b6d09dc2a87ee40
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-06-10 23:39:18 -0600

    Hey I did a callback!

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 extern crate winapi;
 extern crate user32;
 use std::ptr;
-use winapi::winuser::MONITORENUMPROC;
-use winapi::winuser::LPMONITORINFO;
 use winapi::winuser::MONITORINFO;
 use winapi::minwindef::*;
 use winapi::windef::*;
 use std::mem;
-use std::fmt::Debug;
+// use std::fmt::Debug;
 
+#[allow(non_snake_case, unused_variables)]
 unsafe extern "system" fn callback(monitor: HMONITOR, hdc: HDC, work_rect: LPRECT, d: LPARAM) -> BOOL {
-    // let monitorInfoPtr: LPMONITORINFO  = ptr::null_mut();
-    let mut monitorInfo: Box<MONITORINFO> = Box::new(MONITORINFO { cbSize: 0, rcMonitor: RECT {left: 0, top: 0, right: 0, bottom: 0}, rcWork: RECT {left: 0, top: 0, right: 0, bottom: 0}, dwFlags: 0 });
-    monitorInfo.cbSize = mem::size_of::<MONITORINFO>() as u32;
+    let mut monitorInfo: Box<MONITORINFO> = Box::new(MONITORINFO { cbSize: mem::size_of::<MONITORINFO>() as u32, rcMonitor: RECT {left: 0, top: 0, right: 0, bottom: 0}, rcWork: RECT {left: 0, top: 0, right: 0, bottom: 0}, dwFlags: 0 });
     user32::GetMonitorInfoW(monitor, monitorInfo.as_mut());
-    println!("Got monitor info: {:?}", monitorInfo);
     if monitorInfo.dwFlags == 1 {
+        let mut actual_dimensions = Box::new(ScreenDimensions { width: monitorInfo.rcMonitor.right, height: monitorInfo.rcMonitor.bottom });
+        ptr::copy(actual_dimensions.as_mut(), d as *mut ScreenDimensions, mem::size_of::<ScreenDimensions>());
         return 0;
     }
     1
 }
 
 fn main() {
-    unsafe { user32::EnumDisplayMonitors(ptr::null_mut(), ptr::null_mut(), Some(callback), 0); }
-    println!("hi");
+    let mut screen_dimensions: Box<ScreenDimensions> = Box::new(ScreenDimensions { width: 0, height: 0 });
+    let raw = Box::into_raw(screen_dimensions);
+    unsafe { user32::EnumDisplayMonitors(ptr::null_mut(), ptr::null_mut(), Some(callback), raw as i64); }
+    screen_dimensions = unsafe { Box::from_raw(raw) };
+    println!("hi! Your primary screen is {}px by {}px", screen_dimensions.width, screen_dimensions.height);
+}
+
+struct ScreenDimensions {
+    width: i32,
+    height: i32
 }
